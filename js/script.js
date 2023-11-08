adeac6be919bd9fabe4c2760cbe530dbc8157b62
hrvieira <luizhrvieira@outlook.com>
2023-11-07 22:13:44 -0300

    Primeiro commit do meu portfólio. Header pronto!

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("linkedin-link").addEventListener("click", function() {
+  window.location.href = "https://www.linkedin.com/in/luizhrvieira/";
+});
+
+document.getElementById("github-link").addEventListener("click", function() {
+  window.location.href = "https://github.com/hrvieira";
+});
